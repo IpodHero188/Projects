96064fc534c190578d56691e6a01765c11696425
IpodHero188 <DanielMaclin@gmail.com>
2013-11-16 15:06:02 -0600

    The framework for Zenith

---
+++ City_Saver/City_Saver/City_Saver/ObjectClasses/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace City_Saver.ObjectClasses
+{
+    class People
+    {
+        
+        private int health;
+    }
+}
